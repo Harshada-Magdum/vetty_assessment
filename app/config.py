46462e177f802f0c0bfb90208aff899879d9e114
Harshada Magdum <harshadam.in@mouritech.com>
2025-04-12 20:34:08 +0530

    Modified the folder structure as per the coding standards.

---
+++ app/config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+COINGECKO_API = "https://api.coingecko.com/api/v3"
+HEADERS = {"accept": "application/json"}
+API_KEY = os.getenv("API_KEY")  # Now it's loaded from .env
+
+swagger_template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "CoinGecko API Wrapper",
+        "description": "API that wraps selected endpoints from CoinGecko and adds authentication",
+        "version": "1.0"
+    },
+    "securityDefinitions": {
+        "ApiKeyAuth": {
+            "type": "apiKey",
+            "name": "x-api-key",
+            "in": "header"
+        }
+    },
+    "security": [{"ApiKeyAuth": []}]
+}
